344639e71d058ae7684006fe178d9f38c4de30f0
Lei Zhao <lei.zhao@coupa.com>
2020-02-01 10:41:45 -0800

    add by auto checkIn

--- learning/go.mod
+++ learning/go.mod
@@ -1,3 +1,5 @@
 module github.com/leozhao0709/learning
 
 go 1.13
+
+require github.com/gin-gonic/gin v1.5.0 // indirect
--- learning/playground/main.go
+++ learning/playground/main.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Hello Go")
+	fmt.Println("hello world")
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run()
 }
