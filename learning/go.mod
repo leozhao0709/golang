ee903496a175949573074c1dc45ad4bd148a0347
Lei Zhao <lei.zhao@coupa.com>
2020-05-10 18:00:10 -0700

    add by auto checkIn

--- learning/go.mod
+++ learning/go.mod
@@ -1,5 +1,3 @@
 module github.com/leozhao0709/learning
 
-go 1.13
-
-require github.com/gin-gonic/gin v1.5.0 // indirect
+go 1.14
---
+++ learning/src/variable.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+var (
+	a = "a"
+	b = "b"
+)
+
+func main() {
+	c, d := "c", 4
+	fmt.Println(a, b, c, d)
+}
