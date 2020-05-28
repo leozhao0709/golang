bc02b6d8608df6121288e8ae83269e49f043f4d6
Lei Zhao <lei.zhao@coupa.com>
2020-05-28 07:00:15 -0700

    add by auto checkIn

--- learning/src/json/main.go
+++ learning/src/json/main.go
@@ -13,19 +13,52 @@ type monster struct {
 }
 
 func main() {
+	// testStruct()
+	testMap()
+}
+
+func testMap() {
 	// marshal
-	monster1 := &monster{Name: "牛魔王", Age: 0, Skill: "芭蕉扇", Ignore: true}
-	jsonStr, err := json.Marshal(*monster1)
+	var a = make(map[string]interface{})
+	a["name"] = "红孩儿"
+	a["age"] = 30
+	a["address"] = "address"
+
+	jsonStr, err := json.Marshal(a)
 	if err != nil {
-		fmt.Println("json error", err)
+		fmt.Println("marshal error", err)
+		return
 	}
 	fmt.Println(string(jsonStr))
 
 	// unmarshal
+	var a1 map[string]interface{}
+	err = json.Unmarshal(jsonStr, &a1)
+	if err != nil {
+		fmt.Println("error", err)
+		return
+	}
+	fmt.Println(a1)
+}
+
+func testStruct() {
+	// marshal
+	monster1 := &monster{Name: "牛魔王", Age: 0, Skill: "芭蕉扇", Ignore: true}
+	jsonStr, err := json.Marshal(monster1)
+	if err != nil {
+		fmt.Println("json error", err)
+		return
+	}
+	fmt.Println(string(jsonStr))
+
+	// unmarshal
+	// var monster2 = &monster{}
 	var monster2 *monster = &monster{}
 	err = json.Unmarshal(jsonStr, monster2)
 	if err != nil {
 		fmt.Println("error:", err)
+		return
 	}
 	fmt.Println(*monster2)
+	fmt.Println(monster2.Name)
 }
