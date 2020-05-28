b076677d133e1fdc6704a1574e41c4eef8fa16a2
Lei Zhao <lei.zhao@coupa.com>
2020-05-27 18:00:10 -0700

    add by auto checkIn

--- learning/src/file/main.go
+++ learning/src/file/main.go
@@ -12,7 +12,20 @@ import (
 
 func main() {
 	// bufReadLine()
-	ioUtilRead()
+	// ioUtilRead()
+	// bufWrite()
+	copyTest()
+}
+
+func copyTest() {
+	_, filePath, _, _ := runtime.Caller(1)
+	fmt.Println(filePath)
+	dirname := filepath.Dir(filePath)
+	_, err := copy(filepath.Join(dirname, "./test.txt"), filepath.Join(dirname, "./test1.txt"))
+
+	if err != nil {
+		fmt.Println(err)
+	}
 }
 
 func bufReadLine() {
@@ -36,6 +49,8 @@ func bufReadLine() {
 		}
 		if !prefix {
 			fmt.Println(string(bytes))
+		} else {
+			fmt.Print(string(bytes))
 		}
 	}
 
@@ -52,4 +67,44 @@ func ioUtilRead() {
 		fmt.Println("read error", err)
 	}
 	fmt.Println(string(bytes))
+}
+
+func bufWrite() {
+	_, filePath, _, _ := runtime.Caller(1)
+	dirname := filepath.Dir(filePath)
+	filePath = filepath.Join(dirname, "./writeAndCreate.txt")
+
+	file, err := os.OpenFile(filePath, os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+	str := "Hello World!34345\r\n"
+
+	writer.WriteString(str)
+	writer.WriteString(str)
+
+	writer.Flush()
+}
+
+func copy(src, dest string) (int64, error) {
+	srcFile, err := os.Open(src)
+
+	if err != nil {
+		return 0, err
+	}
+	defer srcFile.Close()
+
+	// srcBuffer := bufio.NewReader(srcFile)
+
+	destFile, err := os.OpenFile(dest, os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		return 0, err
+	}
+
+	// destBuff := bufio.NewWriter(destFile)
+	return io.Copy(destFile, srcFile)
 }
