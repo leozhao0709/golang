f76faef8358bb864363d92fadcc671efc491f394
Lei Zhao <lei.zhao@coupa.com>
2020-07-02 07:00:11 -0700

    add by auto checkIn

--- video-server/src/user/routes.go
+++
@@ -1,9 +0,0 @@
-package user
-
-import "github.com/labstack/echo"
-
-// RegisterUserRoute register user route
-func RegisterUserRoute(e *echo.Echo) {
-	e.POST("/user/signup", CreateHandler)
-	e.POST("/user/signin", SignInHandler)
-}
