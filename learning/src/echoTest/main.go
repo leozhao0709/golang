4fc8f142a33cb35a9f1040d3f36609c85943315f
Lei Zhao <lei.zhao@coupa.com>
2020-09-21 18:00:09 -0700

    add by auto checkIn

--- learning/src/echoTest/main.go
+++ learning/src/echoTest/main.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"net/http"
+	"time"
 
-	"github.com/gookit/color"
+	// "github.com/gookit/color"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 	"github.com/labstack/gommon/log"
@@ -23,11 +24,17 @@ func main() {
 	e.GET("/ping", func(c echo.Context) error {
 		// log.Info(color.Cyan.Renderln("this is a info logger"))
 		// log.Info(color.BgCyan.Render("this is a info logger"))
-		log.Info(color.RGB(30, 144, 255).Sprint("this is a info logger"))
+		// log.Info(color.RGB(30, 144, 255).Sprint("this is a info logger"))
 		// log.Info(color.New(color.FgLightBlue, color.BgLightYellow).Renderln("this is a info logger"))
-		return c.JSON(http.StatusOK, map[string]string{
+		result := map[string]string{
 			"message": "pong",
-		})
+		}
+		return c.JSON(http.StatusOK, result)
+	})
+
+	e.GET("/", func(c echo.Context) error {
+		<-time.After(time.Second * 5)
+		return c.String(http.StatusOK, "Hello Golang")
 	})
 	e.Logger.Fatal(e.Start(":8080"))
 }
