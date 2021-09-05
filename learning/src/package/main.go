4f4d1be5dd880329e593daaad39ff44edb97811d
Lei Zhao <lei.zhao@coupa.com>
2021-09-04 18:00:13 -0700

    add by auto checkIn

--- learning/src/package/main.go
+++ learning/src/package/main.go
@@ -4,9 +4,10 @@ import (
 	"fmt"
 
 	"github.com/leozhao0709/learning/src/package/package1"
+	"github.com/leozhao0709/learning/src/package/package2"
 )
 
 func main() {
 	fmt.Println(package1.Test1())
-	// fmt.Println(package1.Test1())
+	fmt.Println(package2.Test())
 }
---
+++ learning/src/package/package2/package2.go
@@ -0,0 +1,7 @@
+package package2
+
+import "github.com/leozhao0709/learning/src/package/package1"
+
+func Test() int {
+	return package1.Test1()
+}
