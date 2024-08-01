bad4ada6df3b19f8404f05c634d1a357893adba4
leozhao0709 <leo.zhao.real@gmail.com>
2024-07-31 21:31:58 -0700

    add by auto checkIn

--- basics/time/main.go
+++ basics/time/main.go
@@ -5,6 +5,20 @@ import (
 	"time"
 )
 
+type JsonTime time.Time
+
+func (t JsonTime) MarshalJSON() ([]byte, error) {
+	stamp := fmt.Sprintf("\"%s\"", time.Time(t).Format("2006-01-02T15:04:05Z07:00"))
+	return []byte(stamp), nil
+}
+
+func (t *JsonTime) UnmarshalJSON(data []byte) error {
+	var err error
+	now, err := time.Parse("2006-01-02T15:04:05Z07:00", string(data))
+	*t = JsonTime(now)
+	return err
+}
+
 func main() {
 	now := time.Now()
 	tomorrow := now.AddDate(0, 0, 1)
